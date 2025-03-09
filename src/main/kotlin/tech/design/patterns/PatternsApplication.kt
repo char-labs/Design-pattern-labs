80d501362caa66b061de733f285f519ad8c0961f
ybchar <uiurihappy@naver.com>
2025-03-09 16:37:38 +0900

    init Project

---
+++ src/main/kotlin/tech/design/patterns/PatternsApplication.kt
@@ -0,0 +1,11 @@
+package tech.design.patterns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PatternsApplication
+
+fun main(args: Array<String>) {
+    runApplication<PatternsApplication>(*args)
+}
---
+++ src/test/kotlin/tech/design/patterns/PatternsApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.design.patterns
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PatternsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
